8879ca46463b4b53db22b893c92a19fe31526646
Hugo-Chiang <statesman1991@gmail.com>
2021-04-22 12:50:05 +0800

    首次上傳

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        navLinks: ['首頁', '關於我們', '最新消息', '服務項目', 'Q&A活動'],
+        circleItems: ['項目一', '項目二', '項目三', '項目四'],
+        listItems: [
+            {
+                ID: '01',
+                date: '08/14',
+                event: '文案一'
+            },
+            {
+                ID: '02',
+                date: '02/14',
+                event: '文案二'
+            },
+            {
+                ID: '03',
+                date: '01/12',
+                event: '文案三'
+            },
+        ]
+    }
+})
