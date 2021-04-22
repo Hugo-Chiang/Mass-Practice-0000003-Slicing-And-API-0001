3507633a997ad7dadd1bca4c2d06093b92fdc8d6
Hugo-Chiang <statesman1991@gmail.com>
2021-04-22 13:30:34 +0800

    break

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ let app = new Vue({
     data: {
         navLinks: ['首頁', '關於我們', '最新消息', '服務項目', 'Q&A活動'],
         circleItems: ['項目一', '項目二', '項目三', '項目四'],
+        communityButtons: {
+            facebook: {
+                src: '/img/facebook_icon.png'
+            },
+            line: {
+                src: '/img/line_icon.png'
+
+            }
+        },
         listItems: [
             {
                 ID: '01',
